b8f942207c6cdd2195bf351c80462fe1b508aa93
win <muhaimin2547@gmail.com>
2023-03-13 00:59:38 +0700

    finnish

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+    static void Main(string[] args) {
+
+        Console.WriteLine("Please input number 1 : ");
+        int number1 = int.Parse (Console.ReadLine());
+         Console.WriteLine("Please input number 2 : ");
+        int number2 = int.Parse (Console.ReadLine());
+         Console.WriteLine("Please input number 3 : ");
+        int number3 = int.Parse (Console.ReadLine());
+        int sum = number1 + number2 + number3;
+
+        Console.WriteLine("sum {0} + {1} + {2} = {3}",number1,number2,number3,sum);
+    }
+}
